5673d91cdefd40df4bb8f2f688517e145c53bac3
Raymond Yang <raymondyangtk@gmail.com>
2023-11-02 16:59:17 -0700

    Add vanilla Unet.

---
+++ model.py
@@ -0,0 +1,115 @@
+import torch
+import torch.nn as nn
+from config import DefaultConfig
+
+config = DefaultConfig()
+
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels,
+                 kernel_size=3, stride=1, padding=1,
+                 has_act=True, activation='relu', has_bn=True):
+        super(ConvBlock, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding)
+        self.has_act = has_act
+        self.activation = None
+        if has_act:
+            self._set_activation(activation)
+        self.has_bn = has_bn
+        if has_bn:
+            self.bn = nn.BatchNorm2d(out_channels)
+
+    def _set_activation(self, activation):
+        if activation == 'relu':
+            self.activation = nn.ReLU(inplace=True)
+        elif activation == 'leakyrelu':
+            self.activation = nn.LeakyReLU(inplace=True)
+        elif activation == 'sigmoid':
+            self.activation = nn.Sigmoid()
+        elif activation == 'tanh':
+            self.activation = nn.Tanh()
+        else:
+            raise ValueError(f"Activation {activation} is not supported.")
+
+    def forward(self, x):
+        x = self.conv(x)
+        if self.has_bn:
+            x = self.bn(x)
+        if self.has_act:
+            x = self.activation(x)
+        return x
+
+
+class UnetBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(UnetBlock, self).__init__()
+        self.conv1 = ConvBlock(in_channels, out_channels)
+        self.conv2 = ConvBlock(out_channels, out_channels)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        return x
+
+
+class Unet(nn.Module):
+    def __init__(self, in_channels=3, out_channels=1, init_features=32):
+        super(Unet, self).__init__()
+        features = init_features
+        self.encoder1 = UnetBlock(in_channels, features)
+        self.encoder2 = UnetBlock(features, features * 2)
+        self.encoder3 = UnetBlock(features * 2, features * 4)
+        self.encoder4 = UnetBlock(features * 4, features * 8)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.bottleneck = UnetBlock(features * 8, features * 16)
+
+        self.upconv4 = nn.ConvTranspose2d(
+            features * 16, features * 8, kernel_size=2, stride=2
+        )
+        self.decoder4 = UnetBlock((features * 8) * 2, features * 8)
+        self.upconv3 = nn.ConvTranspose2d(
+            features * 8, features * 4, kernel_size=2, stride=2
+        )
+        self.decoder3 = UnetBlock((features * 4) * 2, features * 4)
+        self.upconv2 = nn.ConvTranspose2d(
+            features * 4, features * 2, kernel_size=2, stride=2
+        )
+        self.decoder2 = UnetBlock((features * 2) * 2, features * 2)
+        self.upconv1 = nn.ConvTranspose2d(
+            features * 2, features, kernel_size=2, stride=2
+        )
+        self.decoder1 = UnetBlock(features * 2, features)
+        self.conv = nn.Conv2d(
+            in_channels=features, out_channels=out_channels, kernel_size=1
+        )
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        # Encoder
+        enc1 = self.encoder1(x)
+        enc2 = self.encoder2(self.pool(enc1))
+        enc3 = self.encoder3(self.pool(enc2))
+        enc4 = self.encoder4(self.pool(enc3))
+
+        # Bottleneck
+        bottleneck = self.bottleneck(self.pool(enc4))
+
+        # Decoder
+        dec4 = self.upconv4(bottleneck)
+        dec4 = torch.cat((dec4, enc4), dim=1)
+        dec4 = self.decoder4(dec4)
+        dec3 = self.upconv3(dec4)
+        dec3 = torch.cat((dec3, enc3), dim=1)
+        dec3 = self.decoder3(dec3)
+        dec2 = self.upconv2(dec3)
+        dec2 = torch.cat((dec2, enc2), dim=1)
+        dec2 = self.decoder2(dec2)
+        dec1 = self.upconv1(dec2)
+        dec1 = torch.cat((dec1, enc1), dim=1)
+        dec1 = self.decoder1(dec1)
+
+        output = self.conv(dec1)
+        output = self.sigmoid(output)
+
+        return output
