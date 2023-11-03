319853507412f6e13a20b4fbdfe6b929af310a9e
Raymond Yang <raymondyangtk@gmail.com>
2023-11-03 16:16:14 -0700

    Add Dice loss.

---
+++ loss.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+from config import DefaultConfig
+
+config = DefaultConfig()
+
+
+class DiceLoss(nn.Module):
+    def __init__(self, smooth=1.0):
+        super(DiceLoss, self).__init__()
+        self.smooth = smooth
+
+    def forward(self, pred, target):
+        # pred = torch.sigmoid(pred)
+        intersection = (pred * target).sum()
+        dice = (2. * intersection + self.smooth) / (pred.sum() + target.sum() + self.smooth)
+        return 1 - dice
